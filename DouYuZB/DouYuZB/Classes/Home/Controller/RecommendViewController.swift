df4386ca15b8c4dc592a3162cf46a470743430de
gyd <guyindong77@163.com>
2019-06-23 21:37:06 +0800

    无线轮播——UICollectionView

--- DouYuZB/DouYuZB/Classes/Home/Controller/RecommendViewController.swift
+++ DouYuZB/DouYuZB/Classes/Home/Controller/RecommendViewController.swift
@@ -14,6 +14,8 @@ private let kNormalItemH = kItemW * 3 / 4
 private let kPrettyItemH = kItemW * 4 / 3.2
 private let kHeaderViewH : CGFloat = 50
 
+private let kCycleViewH = kScreenH / 5
+
 private let kNormalCellID = "kNormalCellID"
 private let kPrettyCellID = "kPrettyCellID"
 private let kHeaderViewID = "kHeaderViewID"
@@ -45,6 +47,12 @@ class RecommendViewController: UIViewController {
         
         return collectionView
     }()
+    private lazy var cycleView : RecommendCycleView = {
+        let cycleView = RecommendCycleView.recommendCycleView()
+        cycleView.frame = CGRect(x: 0, y: -kCycleViewH, width: kScreenW, height: kCycleViewH)
+        
+        return cycleView
+    }()
 
     // MARK: - 系统回调函数
     override func viewDidLoad() {
@@ -65,6 +73,12 @@ extension RecommendViewController {
     private func setupUI() {
         // 1.将UICollectionView添加到控制器的view中
         view.addSubview(collectionView)
+        
+        // 2.将CycleView添加到UICOllectionView中
+        collectionView.addSubview(cycleView)
+        
+        // 3.设置collectionView的内边距
+        collectionView.contentInset = UIEdgeInsets(top: kCycleViewH, left: 0, bottom: 0, right: 0)
     }
 }
 
