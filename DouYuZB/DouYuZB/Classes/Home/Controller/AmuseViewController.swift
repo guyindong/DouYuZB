7adb26d7e358bb927208431baa83c8a2bf6b9b66
gyd <guyindong77@163.com>
2019-06-29 21:47:27 +0800

    娱乐界面顶部菜单

--- DouYuZB/DouYuZB/Classes/Home/Controller/AmuseViewController.swift
+++ DouYuZB/DouYuZB/Classes/Home/Controller/AmuseViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
+private let kMenmuViewH : CGFloat = 200
+
 class AmuseViewController: BaseAnchorViewController {
 
     // MARK: - 懒加载属性
     private lazy var amuseVM : AmuseViewModel = AmuseViewModel()
+    private lazy var menuView : AmuseMenuView = {
+        let menuView = AmuseMenuView.amuseMenuView()
+        menuView.frame = CGRect(x: 0, y: -kMenmuViewH, width: kScreenW, height: kMenmuViewH)
+        return menuView
+    }()
     
+}
+
+// MARK: - 设置UI界面
+extension AmuseViewController {
+    override func setupUI() {
+        super.setupUI()
+        
+        collectionView.addSubview(menuView)
+        collectionView.contentInset = UIEdgeInsets(top: kMenmuViewH, left: 0, bottom: 0, right: 0)
+    }
 }
 
 // MARK: - 请求数据
@@ -23,6 +40,11 @@ extension AmuseViewController {
         
         amuseVM.loadAmuseData {
             self.collectionView.reloadData()
+            
+            var tempGroups = self.amuseVM.anchorGroups
+            tempGroups.removeFirst()
+            
+            self.menuView.groups = tempGroups
         }
     }
 }
